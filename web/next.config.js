9e102615e85d18d78d41bf0e3f2443e7a3a8ea72
slim1892 <selim1892@outlook.fr>
2026-06-06 21:45:14 +0100

    feat: configure Next.js for Railway deploy

--- web/next.config.js
+++ web/next.config.js
@@ -5,14 +5,16 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['localhost', 'api.mega-supervision.com'],
   },
   async rewrites() {
+    const backendUrl = process.env.BACKEND_URL || 'http://localhost:3000';
     return [
       {
         source: '/api/:path*',
-        destination: `http://localhost:3000/api/v1/:path*`,
+        destination: `${backendUrl}/api/v1/:path*`,
       },
     ];
   },
